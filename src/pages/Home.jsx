0d27c9b5e9d265879d649a7245668b05b20d7c33
eberzina <berzina3633@gmail.com>
2026-03-17 17:15:46 +0200

    Moeve button component to card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import {AdviceCard} from '../components/AdviceCard';
-import {AdviceButton} from '../components/AdviceButton';
 
 export function Home() {
   
   return (
     <main>
       <AdviceCard />
-      <AdviceButton />
     </main>
  )
 }
