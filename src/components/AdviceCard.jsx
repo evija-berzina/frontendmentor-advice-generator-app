1bedf68abab67925bfa0ee354af068c05aa4eca7
eberzina <berzina3633@gmail.com>
2026-03-17 17:16:27 +0200

    Style card component

--- src/components/AdviceCard.jsx
+++ src/components/AdviceCard.jsx
@@ -1,16 +1,18 @@
+import {AdviceButton} from '../components/AdviceButton';
 import DividerMobile from '../assets/images/pattern-divider-mobile.svg';
 import DividerDesktop from '../assets/images/pattern-divider-desktop.svg';
 
 export function AdviceCard() {
   
   return (
-    <section>
-      <h1>Advice #117</h1>
-      <p>It is easy to sit up and take notice, what's difficult is getting up and taking action.</p>
+    <section className='flex flex-col justify-content items-center gap-4 text-center bg-blue-neutral-900 text-lg w-2xs px-4 py-8 rounded-lg relative'>
+      <h1 className='text-green-primary-300 uppercase text-xs tracking-[0.3em]'>Advice #117</h1>
+      <p className='leading-7'><q>It is easy to sit up and take notice, what's difficult is getting up and taking action.</q></p>
       <picture>
         <source media="(min-width: 768px)" srcSet={DividerDesktop} />
-        <img src={DividerMobile} alt="divider pattern" />
+        <img className='pb-5' src={DividerMobile} alt="divider pattern" />
       </picture>
+      <AdviceButton />
     </section>
- )
+  )
 }
