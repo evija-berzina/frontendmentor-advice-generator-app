c9b639ff3ab5eda8d0b6c1fa5ffc00d035173b29
eberzina <berzina3633@gmail.com>
2026-03-17 17:16:15 +0200

    Style button component

--- src/components/AdviceButton.jsx
+++ src/components/AdviceButton.jsx
@@ -3,10 +3,10 @@ import IconDice from '../assets/images/icon-dice.svg';
 export function AdviceButton() {
   
   return (
-    <>
+    <div className='flex justify-center bg-green-primary-300 aspect-square w-13 rounded-full absolute -bottom-6'>
       <button>
-        <img src={IconDice} alt="dice icon" />
+        <img className='w-5' src={IconDice} alt="dice icon" />
       </button>
-    </>
+    </div>
  )
 }
