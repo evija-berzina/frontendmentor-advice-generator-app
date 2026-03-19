c6d76adba3b452d1a145b42ac1ae4342406a9268
eberzina <berzina3633@gmail.com>
2026-03-19 16:55:50 +0200

    Create hover effect for button

--- src/components/AdviceButton.jsx
+++ src/components/AdviceButton.jsx
@@ -3,10 +3,12 @@ import IconDice from '../assets/images/icon-dice.svg';
 export function AdviceButton({dataFetch, isLoading}) {
   
   return (
-    <div className={`flex justify-center bg-green-primary-300 aspect-square w-13 rounded-full absolute -bottom-6 cursor-pointer ${isLoading ? "opacity-75 cursor-default" : ""}`}>
-      <button onClick={dataFetch} disabled={isLoading} className={isLoading ? "cursor-default" : "cursor-pointer"}>
-        <img className={isLoading ? 'animate-spin w-5' : 'w-5'} src={IconDice} alt="dice icon" />
-      </button>
-    </div>
+    <button
+      onClick={dataFetch}
+      disabled={isLoading}
+      className={`flex justify-center items-center bg-green-primary-300 aspect-square w-13 rounded-full absolute -bottom-6 ${isLoading ? "opacity-75 cursor-default" : "cursor-pointer transition-shadow duration-300 hover:shadow-xl/30 hover:shadow-custom"}`}
+    >
+      <img className={`w-5 h-5 ${isLoading ? 'animate-spin w-5' : ''}`} src={IconDice} alt="dice icon" />
+    </button>
  )
 }
