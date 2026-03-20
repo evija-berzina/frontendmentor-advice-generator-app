6874e0f41e3067823ef5434c69e56c4bdfe063c2
eberzina <berzina3633@gmail.com>
2026-03-20 10:48:43 +0200

    Take off hover from mobile layout

--- src/components/AdviceButton.jsx
+++ src/components/AdviceButton.jsx
@@ -7,7 +7,7 @@ export function AdviceButton({dataFetch, isLoading}) {
     type='button'
       onClick={dataFetch}
       disabled={isLoading}
-      className={`flex justify-center items-center bg-green-primary-300 aspect-square w-13 rounded-full absolute -bottom-6 ${isLoading ? "opacity-75 cursor-default" : "cursor-pointer transition-shadow duration-300 hover:shadow-xl/30 hover:shadow-custom"}`}
+      className={`flex justify-center items-center bg-green-primary-300 aspect-square w-13 rounded-full absolute -bottom-6 ${isLoading ? "opacity-75 cursor-default" : "cursor-pointer transition-shadow duration-300 supports-[hover:hover]:hover:shadow-xl/30 supports-[hover:hover]:hover:shadow-custom"}`}
     >
       <img className={`w-5 h-5 ${isLoading ? 'animate-spin w-5' : ''}`} src={IconDice} alt="dice icon" />
     </button>
